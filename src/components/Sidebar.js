821a46a48401e8c931ba9546b55033d50aa356d0
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-15 18:38:28 -0500

    Styled sidebar header

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import '../css/sidebar.css'
-import 
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Sidebar() {
   return (
     <div className="sidebar">
     <div className="sidebar__top">
     <h3>Clever Programmer</h3>
+    <ExpandMoreIcon />
     </div>
+
+
+
     </div>
   )
 }
