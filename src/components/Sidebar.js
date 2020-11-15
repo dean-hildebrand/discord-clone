228de62815a5d02250a7a26d6255524d697d59f3
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-15 18:58:33 -0500

    Styled sidebar and created sidebar channel component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,36 @@
-import React from 'react'
-import '../css/sidebar.css'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import React from "react";
+import "../css/sidebar.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import SidebarChannel from './SidebarChannel'
 
 function Sidebar() {
   return (
     <div className="sidebar">
-    <div className="sidebar__top">
-    <h3>Clever Programmer</h3>
-    <ExpandMoreIcon />
+      <div className="sidebar__top">
+        <h3>Clever Programmer</h3>
+        <ExpandMoreIcon />
+      </div>
+
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+          <ExpandMoreIcon />
+          <h4>Text Channels</h4>
+          </div>
+
+          <AddIcon className="sidebar__addChannel"/>
+        </div>
+        <div className="sidebar__channelsList">
+        <SidebarChannel />
+        <SidebarChannel />
+        <SidebarChannel />
+        
+      </div>
+
+      </div>
     </div>
-
-
-
-    </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
