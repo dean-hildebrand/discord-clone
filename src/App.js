0966e77e47699d9449168a724e486eca93127358
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-17 09:49:21 -0500

    Created login component and ternery for rendering app or login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 import './App.css';
 import Sidebar from './components/Sidebar'
 import Chat from './components/Chat'
+import Login from './components/Login'
+import { selectUser } from './features/userSlice'
 
 function App() {
+  //special react hook to grab state
+  const user = useSelector(selectUser);
   return (
     <div className="app">
+    {user ? (
+      <>
       <Sidebar />
       <Chat />
+      </>
+    ) : (
+      <Login />
+    )}
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/login.css'
+
+function Login() {
+  return (
+    <div className="login">
+    Login Component
+    </div>
+  )
+}
+
+export default Login
