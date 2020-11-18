552d6468489703e4bfdb1bbb6af1eed0ddbca75c
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-18 09:04:02 -0500

    useEffect to map the messages in the db and order by timestamp

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../css/chat.css";
 import ChatHeader from "./ChatHeader";
 import Message from "./Message";
@@ -7,37 +7,61 @@ import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
 import GifIcon from "@material-ui/icons/Gif";
 import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
 import { useSelector } from "react-redux";
-import { selectUser } from '../features/userSlice'
-import { selectChannelId, selectChannelName } from '../features/appSlice'
-
+import { selectUser } from "../features/userSlice";
+import { selectChannelId, selectChannelName } from "../features/appSlice";
+import db from "../app/firebase"
 
 function Chat() {
   const user = useSelector(selectUser);
   const channelId = useSelector(selectChannelId);
   const channelName = useSelector(selectChannelName);
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    if (channelId) {
+      db.collection("channels")
+        .doc(channelId)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+    }
+  }, []);
 
   return (
     <div className="chat">
-      <ChatHeader channelName={channelName}/>
+      <ChatHeader channelName={channelName} />
 
       <div className="chat__messages">
-      <Message />
-      <Message />
-      <Message />
+        {messages.map((message) => (
+          <Message />
+        ))}
       </div>
       <div className="chat__input">
         <AddCircleIcon fontSize="large" />
         <form>
-          <input type="text" placeholder="Message #TestChannel" />
-          <button className="chat__inputButton" type="submit">
+          <input
+            value={input}
+            disabled={!channelId}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder={`Message #${channelName}`}
+          />
+          <button
+            disabled={!channelId}
+            className="chat__inputButton"
+            type="submit"
+          >
             Send Message
           </button>
         </form>
 
         <div className="chat__inputIcons">
-        <CardGiftcardIcon fontSize="large" />
-        <GifIcon fontSize="large" />
-        <EmojiEmotionsIcon fontSize="large" />
+          <CardGiftcardIcon fontSize="large" />
+          <GifIcon fontSize="large" />
+          <EmojiEmotionsIcon fontSize="large" />
         </div>
       </div>
     </div>
